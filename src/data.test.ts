559eeeee467bcb6e25e3a9bbacd1afb8b8ba2d68
Rob Scott <robscott460@gmail.com>
2022-11-19 20:06:48 +0000

    test: add pending tests

---
+++ src/data.test.ts
@@ -0,0 +1,7 @@
+describe("fetchUser", () => {
+  test.todo("it calls the random user api");
+});
+
+describe("generateCsv", () => {
+  test.todo("it takes a json string and returns a csv");
+});
--- src/main.test.ts
+++ src/main.test.ts
@@ -1,37 +1,50 @@
-import { Delays, greeter } from "./main.js";
+describe("getInvisibilityScore", () => {
+  test.todo("it calls calculateInvisibilityScore");
 
-describe("greeter function", () => {
-  const name = "John";
-  let hello: string;
+  test.todo("it calls getInvisibilityStatus");
+});
 
-  let timeoutSpy: jest.SpyInstance;
+describe("calculateInvisibilityScore", () => {
+  describe("for a male", () => {
+    // const cases = [
+    //   { superheroScore: 10, age: 20, score: 0 },
+    //   { superheroScore: 30, age: 20, score: 50 },
+    //   { superheroScore: 25.52, age: 20, score: 27 },
+    //   { superheroScore: 100, age: 20, score: 100 },
+    // ];
 
-  // Act before assertions
-  beforeAll(async () => {
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, "setTimeout");
-
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
-
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
-
-  // Assert if setTimeout was called properly
-  it("delays the greeting by 2 seconds", () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long
+    test.todo(
+      "with superhero score of {$superheroScore} and an age of {$age} it returns {$score}"
     );
   });
 
-  // Assert greeter result
-  it("greets a user with `Hello, {name}` message", () => {
-    expect(hello).toBe(`Hello, ${name}`);
+  describe("for a female", () => {
+    // const cases = [
+    //   { superheroScore: 10, age: 20, score: 0 },
+    //   { superheroScore: 30, age: 20, score: 80 },
+    //   { superheroScore: 25.52, age: 20, score: 44 },
+    //   { superheroScore: 100, age: 20, score: 100 },
+    // ];
+
+    test.todo(
+      "with superhero score of {$superheroScore} and an age of {$age} it returns {$score}"
+    );
   });
+});
+
+describe("getInvisibilityStatus", () => {
+  // const cases = [
+  //   { score: 0, status: "Not invisible"},
+  //   { score: 20, status: "Not invisible"},
+  //   { score: 21, status: "Camouflage"},
+  //   { score: 40, status: "Camouflage"},
+  //   { score: 41, status: "Translucent"},
+  //   { score: 60, status: "Translucent"},
+  //   { score: 61, status: "Transparent"},
+  //   { score: 80, status: "Transparent"},
+  //   { score: 81, status: "Invisible"},
+  //   { score: 100, status: "Invisible"},
+  // ];
+
+  test.todo("for score {$score} it returns {$status}");
 });
